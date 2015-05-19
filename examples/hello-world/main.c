d44cbbb5e54f56035da43086227608182d0d9ac8
Kaspar Schleiser <kaspar@schleiser.de>
2015-05-19 12:13:33 +0200

    strider test 2

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -23,7 +23,7 @@
 
 int main(void)
 {
-    puts("Hello Strider!");
+    puts("Hello Strider! again");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
