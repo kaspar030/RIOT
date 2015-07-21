83428df8046a26bf34bdb3c077b42490d928052d
Kaspar Schleiser <kaspar@schleiser.de>
2015-07-21 10:58:22 +0200

    sys: nanonet: improve ipv6 + udpv6

--- sys/nanonet/nano_udp.c
+++ sys/nanonet/nano_udp.c
@@ -1,7 +1,10 @@
+#include <errno.h>
+
 #include "nano_udp.h"
 #include "nano_ctx.h"
 #include "nano_icmp.h"
 #include "nano_ipv4.h"
+#include "nano_ipv6.h"
 
 #include "byteorder.h"
 
@@ -22,8 +25,15 @@ int udp_handle(nano_ctx_t *ctx, size_t offset)
     uint16_t dst_port = NTOHS(hdr->dst_port);
     ctx->dst_port = dst_port;
 
-    DEBUG("udp: received packet from 0x%08x, src_port %u, dst_port %u\n",
-            (unsigned int) ctx->src_addr.ipv4, ctx->src_port, ctx->dst_port);
+#if ENABLE_DEBUG == 1
+    if (is_ipv4_hdr(ctx->l3_hdr_start)) {
+        DEBUG("udp: received packet from 0x%08x, src_port %u, dst_port %u\n",
+                (unsigned int) ctx->src_addr.ipv4, ctx->src_port, ctx->dst_port);
+    } else {
+        DEBUG("udp: received UDPv6 packet src_port %u, dst_port %u\n",
+                ctx->src_port, ctx->dst_port);
+    }
+#endif
 
     nano_udp_handler_t handler = NULL;
     nano_udp_bind_t *bind = &nano_udp_binds[0];
@@ -42,27 +52,31 @@ int udp_handle(nano_ctx_t *ctx, size_t offset)
         return handler(ctx, offset+sizeof(udp_hdr_t));
     }
     else {
-        if (! (ctx->src_addr.ipv4 || (~ctx->dst_addr.ipv4))) {
-            /* also filter broadcast */
-            DEBUG("udp: unreachable port %u\n", dst_port);
-            icmp_port_unreachable(ctx);
+        if (is_ipv4_hdr(ctx->l3_hdr_start)) {
+            if (! (ctx->src_addr.ipv4 || (~ctx->dst_addr.ipv4))) {
+                /* also filter broadcast */
+                DEBUG("udp: unreachable port %u\n", dst_port);
+                icmp_port_unreachable(ctx);
+            }
+        }
+        else {
+            puts("udp: UDPv6: not filtering broadcast icmp reply");
         }
     }
 
     return 0;
 }
 
-int udp_send(uint32_t dest_ip, uint16_t dst_port, uint16_t src_port, uint8_t *buf, size_t buflen, size_t used) {
-    udp_hdr_t *hdr;
 
-    DEBUG("udp: sending packet to 0x%08x\n", (unsigned int) dest_ip);
+static size_t udp_build_hdr(uint16_t dst_port, uint16_t src_port, uint8_t *buf, size_t buflen, size_t used) {
+    udp_hdr_t *hdr;
 
     /* allocate our header at the end of buf, but before used bytes */
     hdr = (udp_hdr_t *)(buf + buflen - used - sizeof(udp_hdr_t));
 
     if ((uint8_t*) hdr < buf) {
-        DEBUG("udp_send: buffer too small.\n");
-        return -1;
+        DEBUG("udp_build_hdr: buffer too small.\n");
+        return 0;
     }
 
     hdr->src_port = HTONS(src_port);
@@ -70,8 +84,29 @@ int udp_send(uint32_t dest_ip, uint16_t dst_port, uint16_t src_port, uint8_t *bu
 
     hdr->length = HTONS(sizeof(udp_hdr_t) + used);
 
-    /* zero checksum is fine with UDPv4 */
+    /* checksum will be calculated by IP layer */
     hdr->chksum = 0x0;
 
-    return ipv4_send(dest_ip, 0x11, buf, buflen, sizeof(udp_hdr_t) + used);
+    return used + sizeof(udp_hdr_t);
+}
+
+int udp_send(uint32_t dst_ip, uint16_t dst_port, uint16_t src_port, uint8_t *buf, size_t buflen, size_t used) {
+
+    DEBUG("udp: sending packet to 0x%08x\n", (unsigned int) dst_ip);
+
+    if (!udp_build_hdr(dst_port, src_port, buf, buflen, used)) {
+            return -ENOSPC;
+    }
+
+    return ipv4_send(dst_ip, 0x11, buf, buflen, sizeof(udp_hdr_t) + used);
+}
+
+int udp6_send(uint8_t* dst_ip, uint16_t dst_port, uint16_t src_port, uint8_t *buf, size_t buflen, size_t used) {
+    DEBUG("udp: sending udpv6 packet packet\n");
+
+    if (!udp_build_hdr(dst_port, src_port, buf, buflen, used)) {
+            return -ENOSPC;
+    }
+
+    return ipv6_send(dst_ip, IPV6_NEXTHDR_UDP, buf, buflen, sizeof(udp_hdr_t) + used);
 }
