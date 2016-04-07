c20266f875419bfa6d8fba279759075cdc98153d
Kaspar Schleiser <kaspar@schleiser.de>
2016-04-07 23:11:20 +0200

    nanonet: make IPv4 support optional

--- sys/nanonet/include/nano_config.h
+++ sys/nanonet/include/nano_config.h
@@ -1,11 +1,16 @@
 #ifndef NANO_CONFIG_H
 #define NANO_CONFIG_H
 
+#ifndef ENABLE_NANONET_DEBUG
+#define ENABLE_NANONET_DEBUG 1
+#endif
+
 /* should fit the biggest expected packet */
 #define NANONET_RX_BUFSIZE 1536
 
-#ifndef ENABLE_NANONET_DEBUG
-#define ENABLE_NANONET_DEBUG 1
+#ifdef MODULE_NETDEV2_ETH
+#define NANONET_IPV4
+#define NANONET_ETH
 #endif
 
 #endif /* NANO_CONFIG_H */
