d4809a22f38e1b3ca4a23d34965d66cfe27c9c36
Kaspar Schleiser <kaspar@schleiser.de>
2016-12-15 13:42:11 +0100

    squash: make sim8xx test app use shell command

--- tests/driver_sim8xx/main.c
+++ tests/driver_sim8xx/main.c
@@ -19,63 +19,17 @@
  */
 
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 
-#include "fmt.h"
-#include "sim8xx.h"
-#include "xtimer.h"
-
-static sim8xx_t sim8xx;
-static const sim8xx_params_t sim8xx_params = { .uart=SIM8XX_UART, .baudrate=SIM8XX_BAUDRATE };
-
-static char buf[1024];
+#include "shell.h"
+#include "shell_commands.h"
 
 int main(void)
 {
-    puts("sim8xx test application started.");
-    puts("sleeping one second");
-    xtimer_usleep(1000000U);
-    puts("done");
+    puts("SIM8XX device driver test");
 
-    sim8xx_init(&sim8xx, &sim8xx_params);
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
 
-    int res = sim8xx_check_pin(&sim8xx);
-    printf("SIM PIN check: %i\n", res);
-    if (res == 1) {
-#ifdef SIM_PIN
-        res = sim8xx_set_pin(&sim8xx, SIM_PIN);
-        printf("SIM SET: %i\n", res);
-#endif
-        if (res) {
-            goto fail;
-        }
-    }
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
-    while (sim8xx_reg_check(&sim8xx)) {
-        puts("waiting for network registration...");
-        xtimer_usleep(1000000U);
-    }
-
-    res = sim8xx_gprs_init(&sim8xx, SIM8XX_APN);
-    printf("gprs init res: %i\n", res);
-    if (res) {
-        goto fail;
-    }
-
-    res = sim8xx_http_post(&sim8xx, "https://posttestserver.com/post.php?dump",
-            (const uint8_t *) "TEST", 4, (uint8_t*)buf, sizeof(buf));
-
-    printf("http post res: %i\n", res);
-    if (res >= 0) {
-        puts("---- response :");
-        print(buf, res);
-        puts("\n---- done.");
-        return 0;
-    }
-
-fail:
-    puts("error");
-
-    return 1;
+    return 0;
 }
