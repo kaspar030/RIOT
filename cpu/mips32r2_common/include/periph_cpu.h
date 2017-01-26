fc097b4db11b713bc32574b31be3c8a005bc0084
Neil Jones <neil.jones@imgtec.com>
2017-01-26 17:03:21 +0000

    cpu: mips32r2_common: Add boilerplate to empty file
    File is needd else timer-code won't build.
    Boilerplate is needed to pass CI checks.

--- cpu/mips32r2_common/include/periph_cpu.h
+++ cpu/mips32r2_common/include/periph_cpu.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2016, Imagination Technologies Limited and/or its
+ *                 affiliated group companies.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
 /* This file must exist to get timer code to build */
 
 /* No peripherals I/O via JTAG or Bootloader using UHI */
@@ -7,3 +15,12 @@
  * for testing on PFGA systems (like BOARD=mips-malta) with limited
  * or no peripherals
  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
