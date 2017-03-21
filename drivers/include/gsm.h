aa9172e20366d53cd886b33eb0cf91ae5698515f
Kaspar Schleiser <kaspar@schleiser.de>
2017-03-21 16:20:30 +0100

    drivers: gsm: initial commit of GSM modem API

---
+++ drivers/include/gsm.h
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2016 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef GSM_H
+#define GSM_H
+
+#include "at.h"
+#include "mutex.h"
+#include "periph/uart.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GSM_UART_BUFSIZE (1024U)
+#define GSM_SERIAL_TIMEOUT (1000000U)
+
+typedef struct gsm_driver gsm_driver_t;
+
+typedef struct {
+    at_dev_t at_dev;
+    mutex_t mutex;
+    char buf[GSM_UART_BUFSIZE];
+    const gsm_driver_t *driver;
+} gsm_t;
+
+typedef struct {
+    uart_t uart;
+    uint32_t baudrate;
+} gsm_params_t;
+
+struct gsm_driver {
+    int(*get_loc)(gsm_t *dev, char *lon, char *lat);
+    int(*cnet_scan)(gsm_t *dev, char *buf, size_t len);
+    ssize_t(*http_get)(gsm_t *gsmdev, const char *url, uint8_t *resultbuf, size_t len);
+    ssize_t(*http_post)(gsm_t *gsmdev,
+                         const char *url,
+                         const uint8_t *data, size_t data_len,
+                         uint8_t *resultbuf, size_t result_len);
+    int(*time_sync)(gsm_t *dev);
+};
+
+/**
+ * @brief   Initialize GSM device
+ *
+ * @param[in]   gsmdev  device struct to operate on
+ * @param[in]   params  parameter struct used to initialize @p gsmdev
+ *
+ * @returns     0 on success
+ * @returns     <0 otherwise
+ */
+int gsm_init(gsm_t *gsmdev, const gsm_params_t *params);
+
+/**
+ * @brief   Set SIM card PIN
+ *
+ * @param[in]   gsmdev  device struct to operate on
+ * @param[in]   pin     PIN to set
+ *
+ * @returns     0 on success
+ * @returns     -1 on error
+ */
+int gsm_set_pin(gsm_t *gsmdev, const char *pin);
+
+/**
+ * @brief   Test if SIM card needs PIN
+ *
+ * @param[in]   gsmdev  device struct to operate on
+ *
+ * @returns     0 if SIM doesn't need pin
+ * @returns     1 if SIM needs to be unlocked with PIN
+ */
+int gsm_check_pin(gsm_t *gsmdev);
+
+/**
+ * @brief   Initialize GPRS connection
+ *
+ * @param[in]   gsmdev  device struct to operate on
+ * @param[in]   apn     APN used to connect
+ *
+ * @returns     0 on success
+ * @returns     <0 otherwise
+ */
+int gsm_gprs_init(gsm_t *gsmdev, const char *apn);
+
+/**
+ * @brief   Check if modem is registered to network
+ *
+ * @param[in]   gsmdev  device to operate on
+ *
+ * @returns     0 if registered
+ * @returns     1 otherwise
+ */
+int gsm_reg_check(gsm_t *gsmdev);
+
+size_t gsm_reg_get(gsm_t *gsmdev, char *buf, size_t len);
+int gsm_signal_get(gsm_t *gsmdev, unsigned *csq, unsigned *ber);
+int gsm_imei_get(gsm_t *gsmdev, char *buf, size_t len);
+int gsm_iccid_get(gsm_t *gsmdev, char *buf, size_t len);
+uint32_t gsm_gprs_getip(gsm_t *gsmdev);
+void gsm_print_status(gsm_t *gsmdev);
+
+ssize_t gsm_http_get(gsm_t *gsmdev, const char *url, uint8_t *resultbuf, size_t len);
+ssize_t gsm_http_post(gsm_t *gsmdev,
+                         const char *url,
+                         const uint8_t *data, size_t data_len,
+                         uint8_t *resultbuf, size_t result_len);
+
+int gsm_cnet_scan(gsm_t *gsmdev, char *buf, size_t len);
+
+int gsm_gps_get_loc(gsm_t *gsmdev, uint8_t *buf, size_t len);
+size_t gsm_cmd(gsm_t *gsmdev, const char *cmd, uint8_t *buf, size_t len, unsigned timeout);
+int gsm_get_loc(gsm_t *gsmdev, char *lon, char *lat);
+int gsm_off(gsm_t *gsmdev);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GSM_H */
