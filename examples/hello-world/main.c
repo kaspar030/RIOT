8769113994e4ded3e44b41614fdd8d9b34ac2039
Kaspar Schleiser <kaspar@schleiser.de>
2017-06-16 15:16:51 +0200

    REMOVEME: hello world multislot hack

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -21,12 +21,18 @@
 
 #include <stdio.h>
 
+#include "firmware.h"
+#include "cpu.h"
+
 int main(void)
 {
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+    printf("active firmware slot: %i\n", firmware_current_slot());
+
+    firmware_dump_slot_addrs();
 
     return 0;
 }
