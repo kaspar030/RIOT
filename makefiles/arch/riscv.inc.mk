4d809e8d328766fe2d0030225ee172474f2517d2
Kaspar Schleiser <kaspar@schleiser.de>
2018-02-20 14:45:43 +0100

    fixup! build: Add support for SiFive HiFive1 using RISCV cpu.

--- makefiles/arch/riscv.inc.mk
+++ makefiles/arch/riscv.inc.mk
@@ -3,7 +3,7 @@ export TARGET_ARCH ?= riscv-none-embed
 
 # define build specific options
 CFLAGS_CPU   = -march=rv32imac -mabi=ilp32 -mcmodel=medlow -msmall-data-limit=8
-CFLAGS_LINK  = -nostartfiles
+CFLAGS_LINK  = -nostartfiles -ffunction-sections -fdata-sections
 CFLAGS_DBG  ?= -g3 -Og
 #CFLAGS_OPT  ?= -Os
 
@@ -12,7 +12,7 @@ export LINKER_SCRIPT ?= $(CPU_MODEL).ld
 export LINKFLAGS += -T$(LINKER_SCRIPT)
 
 # export compiler flags
-export CFLAGS += $(CFLAGS_CPU) $(CFLAGS_DBG) $(CFLAGS_OPT)
+export CFLAGS += $(CFLAGS_CPU) $(CFLAGS_DBG) $(CFLAGS_OPT) $(CFLAGS_LINK)
 # export assmebly flags
 export ASFLAGS += $(CFLAGS_CPU) $(CFLAGS_DBG)
 # export linker flags
