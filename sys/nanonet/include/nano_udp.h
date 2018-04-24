72fb55d544de0df0d8b387cf5d7c490c86babe0f
Kaspar Schleiser <kaspar@schleiser.de>
2018-04-24 22:58:02 +0200

    nanonet: use clist for udp binds

--- sys/nanonet/include/nano_udp.h
+++ sys/nanonet/include/nano_udp.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include "clist.h"
+
 #include "nano_ctx.h"
 #include "nano_ipv4.h"
 #include "nano_ipv6.h"
@@ -15,14 +17,15 @@ typedef struct __attribute__((packed)) udp_hdr {
     uint16_t chksum;
 } udp_hdr_t;
 
-typedef int (*nano_udp_handler_t) (nano_ctx_t *ctx, size_t offset);
+typedef int (*nano_udp_handler_t) (nano_ctx_t *ctx, size_t offset, ...);
 
 typedef struct nano_udp_bind {
+    clist_node_t next;
     uint16_t port;
     nano_udp_handler_t handler;
 } nano_udp_bind_t;
 
-extern nano_udp_bind_t nano_udp_binds[];
+extern clist_node_t nano_udp_binds;
 
 int udp_handle(nano_ctx_t *ctx, size_t offset);
 int udp_send(const iolist_t *iolist, uint32_t dst_ip, uint16_t dst_port, uint16_t src_port);
