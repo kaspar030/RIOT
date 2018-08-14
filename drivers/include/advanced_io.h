1763d99643b5751f00b54fcb24beea32996e1be5
Philipp Blum <Philipp.Blum@webtrekk.com>
2018-08-14 09:02:01 +0200

    Add license & description

--- drivers/include/advanced_io.h
+++ drivers/include/advanced_io.h
@@ -1,6 +1,36 @@
+/*
+ * Copyright (C) 2017 Philipp-Alexander Blum <philipp-blum@jakiku.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup   advanced_io Advanced I/O Utils
+ * @ingroup    utils
+ * @brief      Advanced I/O Utils
+ *
+ *             The implementation is Arduino compatible with a different API.
+ * @{
+ *
+ * @file
+ * @brief      Advanced I/O Utils
+ *
+ * @author     Philipp-Alexander Blum <philipp-blum@jakiku.de>
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {
     MSBFIRST,
     LSBFIRST
 } advanced_io_bit_order_t;
 
 uint8_t advanced_io_shift_in(gpio_t dataPin, gpio_t clockPin, advanced_io_bit_order_t bitOrder);
+
+#ifdef __cplusplus
+}
+#endif
