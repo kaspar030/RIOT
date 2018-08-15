68d6bd91d7fa11739cfaaa1e516747b602bc60ae
Philipp Blum <philipp-blum@jakiku.de>
2018-08-15 04:58:54 +0200

    Update advanced io

--- drivers/advanced_io/advanced_io.c
+++ drivers/advanced_io/advanced_io.c
@@ -8,25 +8,13 @@
 
 #include "periph/gpio.h"
 
-typedef enum {
-    MSBFIRST,
-    LSBFIRST
-} advanced_io_bit_order_t;
-
-uint8_t advanced_io_shift_in(gpio_t data_pin, gpio_t clock_pin, advanced_io_bit_order_t bit_order){
+uint8_t advanced_io_shift_in(gpio_t data_pin, gpio_t clock_pin){
     uint8_t byte = 0x00;
 
-    for(int i = 0; i <= 8; i++){
+    for(unsigned int i = 0; i <= 8; i++){
         gpio_set(clock_pin);
         if(gpio_read(data_pin) > 0){
-            if(bit_order == MSBFIRST){
-                byte = byte | 0x01 << (8 - i);
-            }
-            else if(bit_order == LSBFIRST){
-                byte = byte | 0x01 << i;
-            } else {
-                return '\0';
-            }
+            byte = byte | 0x01 << (8 - i);
         }
         gpio_clear(clock_pin);
     }
