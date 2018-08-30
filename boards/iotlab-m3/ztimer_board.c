9616ff9b8e7b6805685628fa7550feeae6c21607
Kaspar Schleiser <kaspar@schleiser.de>
2018-08-30 21:59:48 +0200

    fixup! WIP ztimer board configs

--- boards/iotlab-m3/ztimer_board.c
+++ boards/iotlab-m3/ztimer_board.c
@@ -12,8 +12,8 @@ static ztimer_rtt_t _ztimer_rtt;
 static ztimer_convert_t _ztimer_rtt_convert;
 static ztimer_extend_t _ztimer_rtt_extend;
 
-ztimer_instance_t *const ZTIMER_USEC = (ztimer_instance_t*) &_ztimer_periph;
-ztimer_instance_t *const ZTIMER_MSEC = (ztimer_instance_t*) &_ztimer_rtt_extend;
+ztimer_dev_t *const ZTIMER_USEC = (ztimer_dev_t*) &_ztimer_periph;
+ztimer_dev_t *const ZTIMER_MSEC = (ztimer_dev_t*) &_ztimer_rtt_extend;
 
 void ztimer_board_init(void)
 {
@@ -22,6 +22,6 @@ void ztimer_board_init(void)
     DEBUG("ztimer_board_init(): ZTIMER_US diff=%"PRIu32"\n", _ztimer_periph.adjust);
 
     ztimer_rtt_init(&_ztimer_rtt);
-    ztimer_convert_init(&_ztimer_rtt_convert, (ztimer_instance_t*)&_ztimer_rtt, &ztimer_convert_ops_1000to1024);
-    ztimer_extend_init(&_ztimer_rtt_extend, (ztimer_instance_t*)&_ztimer_rtt_convert, 22);
+    ztimer_convert_init(&_ztimer_rtt_convert, (ztimer_dev_t*)&_ztimer_rtt, &ztimer_convert_ops_1000to1024);
+    ztimer_extend_init(&_ztimer_rtt_extend, (ztimer_dev_t*)&_ztimer_rtt_convert, 22);
 }
