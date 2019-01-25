430b5ab8048b212897e311dd9892e3bac5b7ba7a
Kaspar Schleiser <kaspar@schleiser.de>
2019-01-25 09:19:41 +0100

    fixup! Simplify & counting down instead of up

--- drivers/advanced_io/advanced_io.c
+++ drivers/advanced_io/advanced_io.c
@@ -8,13 +8,14 @@
 
 #include "periph/gpio.h"
 
-uint8_t advanced_io_shift_in(gpio_t data_pin, gpio_t clock_pin){
+uint8_t advanced_io_shift_in(gpio_t data_pin, gpio_t clock_pin)
+{
     uint8_t byte = 0x00;
 
-    for(unsigned int i = 7; i >= 0; i--){
+    for (unsigned int i = 8; i > 0; i--) {
         gpio_set(clock_pin);
-        if(gpio_read(data_pin) > 0){
-            byte |= (1 << i);
+        if (gpio_read(data_pin) > 0) {
+            byte |= (1 << (i - 1));
         }
         gpio_clear(clock_pin);
     }
