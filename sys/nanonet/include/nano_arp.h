2805c0d3a1418d24f4ff4a0772afa4d49cb064d2
Kaspar Schleiser <kaspar@schleiser.de>
2019-02-19 14:10:15 +0100

    nanonet: arp: move stuff from header to .c

--- sys/nanonet/include/nano_arp.h
+++ sys/nanonet/include/nano_arp.h
@@ -5,25 +5,6 @@
 #include "nano_ctx.h"
 #include "nano_dev.h"
 
-#define NANO_ARP_CACHE_SIZE 16
-
-typedef struct  __attribute__((packed)) {
-    uint32_t arp_ipv4_types;
-    uint16_t arp_ipv4_lengths;
-    uint16_t arp_ipv4_op;
-    uint8_t src_mac[6];
-    uint32_t src_ip;
-    uint8_t dst_mac[6];
-    uint32_t dst_ip;
-} arp_pkt_t;
-
-typedef struct {
-    uint32_t ip;
-    uint8_t mac[6];
-    uint16_t since;
-    nano_dev_t* dev;
-} arp_cache_entry_t;
-
 int arp_handle(nano_ctx_t *ctx, size_t offset);
 
 int arp_cache_get(nano_dev_t *dev, uint32_t dest_ip, uint8_t *mac_addr_out);
