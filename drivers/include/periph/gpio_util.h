85854c5f89ad183a348491138daaed9e419bb78e
Kaspar Schleiser <kaspar@schleiser.de>
2019-03-05 20:59:29 +0100

    fixup! Change description

--- drivers/include/periph/gpio_util.h
+++ drivers/include/periph/gpio_util.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Philipp-Alexander Blum <philipp-blum@jakiku.de>
+ * Copyright (C) 2019 Philipp-Alexander Blum <philipp-blum@jakiku.de>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -8,13 +8,13 @@
 
 /**
  * @defgroup   gpio_util GPIO I/O Utils
- * @ingroup    utils
- * @brief      GPIO I/O Utils. Same functionality as the Arduino shiftIn() in Advanced I/O.
+ * @ingroup    drivers_periph_gpio
+ * @brief      GPIO I/O utility functions
  *
  * @{
  *
  * @file
- * @brief      GPIO I/O Utils
+ * @brief      GPIO I/O utility function implementations
  *
  * @author     Philipp-Alexander Blum <philipp-blum@jakiku.de>
  */
@@ -26,14 +26,16 @@ extern "C" {
 #endif
 
 /**
+ * @brief Shift in a byte from data_pin, create clock pulses on clock_pin
  *
- * @brief shift in a byte from a given data_pin. Create clock pulses on clock_pin. MSB first.
- * @param data_pin
- * @param clock_pin
- * @return the resulting uint8_t of the shift.
+ * This funtion has the same functionality as the Arduino shiftIn() in Advanced
+ * I/O.
+ *
+ * @param[in]   data_pin    Pin to read data from
+ * @param       clock_pin   Pin to create clock pulses on
+ * @return      the resulting uint8_t of the shift (MSB first)
  */
 uint8_t gpio_util_shiftin(gpio_t data_pin, gpio_t clock_pin);
-
 
 #ifdef __cplusplus
 }
