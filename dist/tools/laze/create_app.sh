11204dd165ac5c004327621a7c1b61c1c61ed369
Kaspar Schleiser <kaspar@schleiser.de>
2019-03-19 23:46:57 +0100

    laze: add helper scripts

---
+++ dist/tools/laze/create_app.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. ${RIOTBASE}/dist/tools/laze/common.sh
+
+eval "$(RIOTBASE=${RIOTBASE}/dist/tools/laze make all)"
+
+laze create --type=app \
+    $(arglist --depends "${DEPENDS}") \
+    $(arglist --uses "${USES}") \
+    $(arglist --sources "$(ls -Imain.c | grep -E '(\.c|\.cpp)$')")
+
+[ -n "$CFLAGS" ] && {
+    {
+        echo "    global_vars:"
+        echo "        CFLAGS:"
+        for var in $CFLAGS; do
+            a="$(echo $var | cut -d= -f1)"
+            b="$(echo $var | cut -s -d= -f2)"
+            [ -n "$b" ] && echo "            - $a=\"$b\"" \
+                 || echo "            - $a"
+        done
+    } >> build.yml
+}
