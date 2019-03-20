0c4f6b93affb0962915cf3fbfd301e6402068e3d
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-03-20 21:49:25 +0100

    makefiles/suit*: refactor build system integration + add support for http ota-server

---
+++ makefiles/suit.http.inc.mk
@@ -0,0 +1,29 @@
+# Publish ID can be whatever the user prefer. The OTA server will take care of
+# it.
+SUIT_PUBLISH_ID ?= $(BOARD)_$(APPLICATION)
+
+# Required for manifest generation
+SUIT_COAP_SERVER ?= [::1]
+SUIT_COAP_ROOT ?= coap://$(SUIT_COAP_SERVER)/$(SUIT_PUBLISH_ID)
+
+# Some requirements to publish and notify updates to the server
+SUIT_OTA_SERVER_BASEDIR ?=
+SUIT_OTA_SERVER_URL ?= http://localhost:8080
+SUIT_OTA_SERVER_PUBLISH_EP ?= publish
+SUIT_OTA_SERVER_NOTIFY_EP ?= notify
+
+# Only 2 manifest files are required for the build system
+SUIT_MANIFESTS := $(SLOT0_SUIT_MANIFEST) \
+                  $(SLOT1_SUIT_MANIFEST) \
+
+ifeq (,$(SUIT_OTA_SERVER_BASEDIR))
+  $(error Please provide the path to the ota-server source base directory.)
+endif
+
+OTA_CLIENT_CMD = python3 $(SUIT_OTA_SERVER_BASEDIR)/client/otaclient.py
+
+suit/publish: $(SUIT_MANIFESTS) $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
+	$(Q)$(OTA_CLIENT_CMD) --publish-id $(SUIT_PUBLISH_ID) --publish $^
+
+suit/notify:
+	$(Q)$(OTA_CLIENT_CMD) --publish-id $(SUIT_PUBLISH_ID) --notify $(SUIT_CLIENT)
