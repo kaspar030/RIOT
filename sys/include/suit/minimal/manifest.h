ee1f5f96266f112c48b8bc292dc30f624e663cfb
Kaspar Schleiser <kaspar@schleiser.de>
2019-03-27 18:07:03 +0100

    QIP minimal manifest: encode urls, use uuid

--- sys/include/suit/minimal/manifest.h
+++ sys/include/suit/minimal/manifest.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "uuid.h"
+
 #define SUIT_MINIMAL_SLOT_NUMOF 2
 /* dependend on used algorithms... */
 #define SUIT_MINIMAL_DIGEST_SIZE        16
@@ -13,46 +15,47 @@
 
 typedef struct __attribute__ ((packed)) {
     uint32_t size;
-    uint8_t url[SUIT_MINIMAL_URL_MAXLEN];
     uint8_t digest[SUIT_MINIMAL_DIGEST_SIZE];
 } suit_minimal_slot_t;
 
 typedef struct __attribute__ ((packed)) {
     uint8_t signature[SUIT_MINIMAL_SIGNATURE_SIZE];
     uint32_t seq_nr;
-    uint32_t vendor_id;
-    uint32_t class_id;
-    uint32_t device_id;
+    uuid_t vendor_id;
+    uuid_t class_id;
+    uuid_t device_id;
     uint8_t manifest_version;
+    uint8_t encoded_urls_len;
+    uint8_t reserved[2];
     suit_minimal_slot_t slots[SUIT_MINIMAL_SLOT_NUMOF];
-    uint8_t url_len[SUIT_MINIMAL_SLOT_NUMOF];
+    uint8_t encoded_urls[];
 } suit_minimal_manifest_t;
 
-static inline size_t suit_minimal_manifest_signlen(const suit_minimal_manifest_t *manifest)
+static inline size_t suit_minimal_manifest_signlen(
+    const suit_minimal_manifest_t *manifest)
 {
-    (void)manifest;
-    return sizeof(suit_minimal_manifest_t) - SUIT_MINIMAL_SIGNATURE_SIZE;
+    return sizeof(suit_minimal_manifest_t) + manifest->encoded_urls_len -
+           SUIT_MINIMAL_SIGNATURE_SIZE;
 }
 
-static inline void *suit_minimal_manifest_signstart(suit_minimal_manifest_t *manifest)
+static inline void *suit_minimal_manifest_signstart(
+    suit_minimal_manifest_t *manifest)
 {
     return &manifest->seq_nr;
 }
 
-static inline ssize_t suit_minimal_url_get(const suit_minimal_manifest_t *manifest,
-                                       uint8_t *urlbuf, size_t buflen, unsigned target_slot)
+ssize_t suit_minimal_encode_urls(uint8_t *outbuf, size_t outbuf_len,
+                                uint8_t *urls[], size_t urls_num);
+
+ssize_t suit_minimal_decode_url(uint8_t *out, size_t out_len, const uint8_t *encoded,
+                               size_t n);
+
+static inline ssize_t suit_minimal_url_get(
+    const suit_minimal_manifest_t *manifest, uint8_t *urlbuf, size_t buflen,
+    unsigned target_slot)
 {
-    if (manifest->url_len[target_slot] > (buflen + 1))  {
-        return -1;
-    }
-
-    /* copy url */
-    memcpy(urlbuf, manifest->slots[target_slot].url, manifest->url_len[target_slot]);
-
-    /* null terminate */
-    urlbuf[manifest->url_len[target_slot]] = '\0';
-
-    return manifest->url_len[target_slot] + 1;
+    return suit_minimal_decode_url(urlbuf, buflen, manifest->encoded_urls,
+                                   target_slot);
 }
 
 void suit_minimal_manifest_print(const suit_minimal_manifest_t *m);
--- sys/suit/minimal/manifest.c
+++ sys/suit/minimal/manifest.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
 #include "suit/minimal/manifest.h"
 
 void suit_minimal_manifest_print(const suit_minimal_manifest_t *m)
@@ -9,4 +14,73 @@ void suit_minimal_manifest_print(const suit_minimal_manifest_t *m)
         suit_minimal_url_get(m, url, SUIT_MINIMAL_URL_MAXLEN, slot);
         printf("url slot %u: \"%s\"\n", slot, url);
     }
+}
+
+ssize_t suit_minimal_encode_urls(uint8_t *outbuf, size_t outbuf_len,
+                                uint8_t *urls[], size_t urls_num)
+{
+    size_t prefix_len;
+    uint8_t *outbuf_pos;
+
+    /* count number of prefix chars all urls have in common */
+    for (prefix_len = 0;; prefix_len++) {
+        uint8_t c = urls[0][prefix_len];
+        if (!c) {
+            break;
+        }
+
+        for (size_t i = 1; i < urls_num; i++) {
+            if (urls[i][prefix_len] != c) {
+                goto done;
+            }
+        }
+    }
+
+done:
+
+    if ((prefix_len + 3) > outbuf_len) {
+        return -ENOSPC;
+    }
+
+    outbuf_pos = outbuf;
+    *outbuf_pos++ = (uint8_t)prefix_len;
+    memcpy(outbuf_pos, urls[0], prefix_len);
+
+    outbuf_pos += prefix_len;
+    for (size_t i = 0; i < urls_num; i++) {
+        size_t to_copy = strlen((char *)urls[0]) - prefix_len;
+        *outbuf_pos++ = (uint8_t)to_copy;
+        memcpy(outbuf_pos, urls[i] + prefix_len, to_copy);
+        outbuf_pos += to_copy;
+    }
+
+    return outbuf_pos - outbuf;
+}
+
+ssize_t suit_minimal_decode_url(uint8_t *out, size_t out_len, const uint8_t *encoded,
+                               size_t n)
+{
+    uint8_t len = *encoded++;
+    size_t outlen = len;
+
+    if (out_len < len) {
+        return -ENOSPC;
+    }
+
+    memcpy(out, encoded, len);
+    out += len;
+    encoded += len;
+    for (unsigned i = 0; i < n; i++) {
+        len = *encoded++;
+        encoded += len;
+    }
+    len = *encoded++;
+    outlen += len;
+
+    if ((out_len + 1) < len) {
+        return -ENOSPC;
+    }
+
+    strncpy((char *)out, (char *)encoded, len);
+    return outlen;
 }
