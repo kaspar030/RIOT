f0cffafc914864d8306621b8efdd35b3c3c526ea
Kaspar Schleiser <kaspar@schleiser.de>
2019-04-10 10:53:21 +0200

    boards: native: guard motor driver simulation code

--- boards/native/include/board.h
+++ boards/native/include/board.h
@@ -127,6 +127,7 @@ extern mtd_dev_t *mtd0;
 /** @} */
 #endif
 
+#ifdef MODULE_MOTOR_DRIVER
 /**
  * @brief Simulate QDEC on motor_set() calls
  *
@@ -182,6 +183,7 @@ static const motor_driver_config_t motor_driver_config[] = {
 
 #define MOTOR_DRIVER_NUMOF           ARRAY_SIZE(motor_driver_config)
 /** @} */
+#endif
 
 #endif /* __cplusplus */
 
