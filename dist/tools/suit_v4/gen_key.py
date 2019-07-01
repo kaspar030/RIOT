2c390d8476bc689f990f6509fa8f22e149116a0c
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-07-01 22:18:30 +0200

    tools/suit_v4/gen_key: global cleanup

--- dist/tools/suit_v4/gen_key.py
+++ dist/tools/suit_v4/gen_key.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
-import ed25519
 import sys
-signing_key, verifying_key = ed25519.create_keypair()
-open(sys.argv[1], "wb").write(signing_key.to_bytes())
-vkey_hex = verifying_key.to_ascii(encoding="hex")
-open(sys.argv[2], "wb").write(verifying_key.to_bytes())
-print("the public key is", vkey_hex)
+import ed25519
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("usage: gen_key.py <secret filename> <public filename>")
+        sys.exit(1)
+
+    _signing_key, _verifying_key = ed25519.create_keypair()
+    with open(sys.argv[1], "wb") as f:
+        f.write(_signing_key.to_bytes())
+
+    with open(sys.argv[2], "wb") as f:
+        f.write(_verifying_key.to_bytes())
+
+    vkey_hex = _verifying_key.to_ascii(encoding="hex")
+    print("Generated public key: '{}'".format(vkey_hex.decode()))
+
+
+if __name__ == '__main__':
+    main()
