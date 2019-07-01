0278fe7fe04aa027da6db70a8028b78214eb70ee
Kaspar Schleiser <kaspar@schleiser.de>
2019-07-01 11:22:48 +0200

    suit: change to ssh-like key file naming scheme

--- makefiles/suit.v4.inc.mk
+++ makefiles/suit.v4.inc.mk
@@ -20,33 +20,44 @@ SUIT_DEVICE_ID ?= $(BOARD)
 #
 # SUIT encryption keys
 #
+
+# Specify key to use.
+# Will use $(SUIT_KEY_DIR)/$(SUIT_KEY) $(SUIT_KEY_DIR)/$(SUIT_KEY).pub as
+# private/public key files, similar to how ssh names its key files.
+SUIT_KEY ?= default
+
 ifeq (1, $(RIOT_CI_BUILD))
   SUIT_KEY_DIR ?= $(BINDIR)
 else
   SUIT_KEY_DIR ?= $(RIOTBASE)/keys
 endif
 
-SUIT_KEY ?= $(SUIT_KEY_DIR)/secret.key
-SUIT_PUB ?= $(SUIT_KEY_DIR)/public.key
+SUIT_SEC ?= $(SUIT_KEY_DIR)/$(SUIT_KEY)
+SUIT_PUB ?= $(SUIT_KEY_DIR)/$(SUIT_KEY).pub
 
 SUIT_PUB_HDR = $(BINDIR)/riotbuild/public_key.h
 SUIT_PUB_HDR_DIR = $(dir $(SUIT_PUB_HDR))
 CFLAGS += -I$(SUIT_PUB_HDR_DIR)
 BUILDDEPS += $(SUIT_PUB_HDR)
 
-$(SUIT_KEY) $(SUIT_PUB):
+$(SUIT_SEC) $(SUIT_PUB):
 	@echo suit: generating key pair in $(SUIT_KEY_DIR)
 	@mkdir -p $(SUIT_KEY_DIR)
-	@$(RIOTBASE)/dist/tools/suit_v4/gen_key.py $(SUIT_KEY) $(SUIT_PUB)
+	@$(RIOTBASE)/dist/tools/suit_v4/gen_key.py $(SUIT_SEC) $(SUIT_PUB)
 
-$(SUIT_PUB_HDR): $(SUIT_PUB)
+# set FORCE so switching between keys using "SUIT_KEY=foo make ..."
+# triggers a rebuild even if the new key would otherwise not (because the other
+# key's mtime is too far back).
+$(SUIT_PUB_HDR): $(SUIT_PUB) FORCE
 	@mkdir -p $(SUIT_PUB_HDR_DIR)
-	@cd $(dir $(SUIT_PUB)) && xxd -i $(notdir $(SUIT_PUB)) > $@
+	@cp $(SUIT_PUB) $(SUIT_PUB_HDR_DIR)/public.key
+	@cd $(SUIT_PUB_HDR_DIR) && xxd -i public.key \
+	  | '$(LAZYSPONGE)' $(LAZYSPONGE_FLAGS) '$@'
 
-suit/genkey: $(SUIT_KEY) $(SUIT_PUB)
+suit/genkey: $(SUIT_SEC) $(SUIT_PUB)
 
 suit/delkeys:
-	rm -f $(SUIT_KEY) $(SUIT_PUB)
+	rm -f $(SUIT_SEC) $(SUIT_PUB)
 
 #
 $(SUIT_MANIFEST): $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
@@ -62,7 +73,7 @@ $(SUIT_MANIFEST): $(SLOT0_RIOT_BIN) $(SLOT1_RIOT_BIN)
 
 $(SUIT_MANIFEST_SIGNED): $(SUIT_MANIFEST)
 	$(RIOTBASE)/dist/tools/suit_v4/sign-04.py \
-	  $(SUIT_KEY) $(SUIT_PUB) $< $@
+	  $(SUIT_SEC) $(SUIT_PUB) $< $@
 
 $(SUIT_MANIFEST_LATEST): $(SUIT_MANIFEST)
 	@ln -f -s $< $@
