01c39f3dbe5ff94f0037d4ee2921ffa72b0fb988
Kaspar Schleiser <kaspar@schleiser.de>
2019-09-24 14:18:48 +0200

    fixup! ...

--- sys/libc/include/stdlib.h
+++ sys/libc/include/stdlib.h
@@ -5,7 +5,8 @@
 
 long int strtol(const char *nptr, char **endptr, int base);
 unsigned long int strtoul(const char *nptr, char **endptr, int base);
-static inline atoi(const char *nptr) { return strtol(nptr, NULL, 10);}
+static inline int atoi(const char *nptr) { return strtol(nptr, NULL, 10);}
+static inline long atol(const char *nptr) { return strtol(nptr, NULL, 10);}
 
 
 #endif /* STDLIB_H */
