38e693a246454cc96cbc14a2be55b4ac2c7bb942
Kaspar Schleiser <kaspar@schleiser.de>
2020-01-31 11:29:34 +0100

    nanonet: introduce nano_util_calcsum_iolist()

--- sys/nanonet/nano_util.c
+++ sys/nanonet/nano_util.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 
 #include "fmt.h"
+#include "iolist.h"
 
 /* calcsum - used to calculate IP and ICMP header checksums using
  * one's compliment of the one's compliment sum of 16 bit words of the header
@@ -11,6 +12,7 @@ uint16_t nano_util_calcsum(uint32_t sum, const uint8_t *buffer, size_t len)
 {
     /* add all even 16-bit words to the checksum */
     while (len > 1) {
+        /* TODO: fix possibly unaligned access */
         sum += *(uint16_t *)buffer;
         len -= 2;
         buffer += 2;
@@ -19,6 +21,38 @@ uint16_t nano_util_calcsum(uint32_t sum, const uint8_t *buffer, size_t len)
     /* add the last missing byte in case of an un-even byte-count */
     if (len) {
         uint8_t tmp[2] = { *buffer, 0 };
+        sum += *(uint16_t *)tmp;
+    }
+
+    /* Fold 32-bit sum to 16 bits */
+    while (sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+
+    return sum;
+}
+
+uint16_t nano_util_calcsum_iolist(uint32_t sum, const iolist_t *iolist)
+{
+    size_t offset = 0;
+    uint8_t tmp[2] = {0};
+    while (iolist) {
+        uint8_t *pos = iolist->iol_base;
+        for (size_t i = 0; i < iolist->iol_len; i++, pos++, offset++) {
+            /* even bytes go to tmp[0], odd bytes to tmp[1] */
+            tmp[offset & 1] = *pos;
+
+            /* if byte was even and offset != 0, we got a pair. add to sum. */
+            if ((!offset & 1) && offset) {
+                sum += *(uint16_t *)tmp;
+            }
+        }
+        iolist = iolist->iol_next;
+    }
+
+    /* add the last missing byte in case of an un-even byte-count */
+    if (!(offset & 1)) {
+        tmp[1] = 0;
         sum += *(uint16_t *)tmp;
     }
 
