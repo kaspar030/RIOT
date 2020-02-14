daf82f83970e7e9447c86ee6dc3813fda17ff795
Kaspar Schleiser <kaspar@schleiser.de>
2020-02-14 11:14:03 +0100

    fixup! nanonet: introduce nano_util_calcsum_iolist()

--- sys/net/nano/util.c
+++ sys/net/nano/util.c
@@ -5,6 +5,9 @@
 #include "fmt.h"
 #include "iolist.h"
 
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
 /* calcsum - used to calculate IP and ICMP header checksums using
  * one's compliment of the one's compliment sum of 16 bit words of the header
  */
@@ -12,6 +15,7 @@ uint16_t nano_util_calcsum(uint32_t sum, const uint8_t *buffer, size_t len)
 {
     /* add all even 16-bit words to the checksum */
     while (len > 1) {
+        DEBUG("%s(): sum=0x%08x, len=%zu, add=0x%04x\n", __func__,sum, len, *(uint16_t *)buffer);
         /* TODO: fix possibly unaligned access */
         sum += *(uint16_t *)buffer;
         len -= 2;
@@ -21,6 +25,7 @@ uint16_t nano_util_calcsum(uint32_t sum, const uint8_t *buffer, size_t len)
     /* add the last missing byte in case of an un-even byte-count */
     if (len) {
         uint8_t tmp[2] = { *buffer, 0 };
+        DEBUG("%s(): sum=0x%08x, len=%zu, add=0x%04x\n", __func__, sum, len, *(uint16_t *)tmp);
         sum += *(uint16_t *)tmp;
     }
 
@@ -29,21 +34,27 @@ uint16_t nano_util_calcsum(uint32_t sum, const uint8_t *buffer, size_t len)
         sum = (sum & 0xffff) + (sum >> 16);
     }
 
+    DEBUG("%s(): return sum=0x%08x\n", __func__, sum);
+
     return sum;
 }
 
 uint16_t nano_util_calcsum_iolist(uint32_t sum, const iolist_t *iolist)
 {
+
     size_t offset = 0;
     uint8_t tmp[2] = {0};
     while (iolist) {
         uint8_t *pos = iolist->iol_base;
         for (size_t i = 0; i < iolist->iol_len; i++, pos++, offset++) {
+            DEBUG("%s(): sum=0x%08x, i=%zu, *pos=0x%02x, offset=%zu, offset&1=%i\n",
+                   __func__, sum, i, *pos, offset, offset & 1);
             /* even bytes go to tmp[0], odd bytes to tmp[1] */
             tmp[offset & 1] = *pos;
 
-            /* if byte was even and offset != 0, we got a pair. add to sum. */
-            if ((!offset & 1) && offset) {
+            /* if byte was odd, we got a pair. add to sum. */
+            if (offset & 1) {
+                DEBUG("%s(): sum=0x%08x, add=0x%04x, offset=%zu\n", __func__, sum, *(uint16_t *)tmp, offset);
                 sum += *(uint16_t *)tmp;
             }
         }
@@ -53,6 +64,7 @@ uint16_t nano_util_calcsum_iolist(uint32_t sum, const iolist_t *iolist)
     /* add the last missing byte in case of an un-even byte-count */
     if (!(offset & 1)) {
         tmp[1] = 0;
+        DEBUG("%s(): sum=0x%08x, add=0x%04x\n", __func__, sum, *(uint16_t *)tmp);
         sum += *(uint16_t *)tmp;
     }
 
@@ -60,6 +72,8 @@ uint16_t nano_util_calcsum_iolist(uint32_t sum, const iolist_t *iolist)
     while (sum >> 16) {
         sum = (sum & 0xffff) + (sum >> 16);
     }
+
+    DEBUG("%s(): return sum=0x%08x\n", __func__, sum);
 
     return sum;
 }
