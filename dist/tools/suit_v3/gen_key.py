5f804680c9e0cdbbd38719ff2d62b938b5e65377
Kaspar Schleiser <kaspar@schleiser.de>
2020-02-18 15:55:57 +0100

    dist/tools/suit_v3/gen_key.py: create PEM file

--- dist/tools/suit_v3/gen_key.py
+++ dist/tools/suit_v3/gen_key.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 #
-# Copyright (C) 2019 Inria
-#               2019 FU Berlin
+# Copyright (C) 2020 Kaspar Schleiser <kaspar@schleiser.de>
+#               2020 Inria
+#               2020 Freie Universität Berlin
+    
+
 #
 # This file is subject to the terms and conditions of the GNU Lesser
 # General Public License v2.1. See the file LICENSE in the top level
@@ -10,23 +13,25 @@
 #
 
 import sys
-import ed25519
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
 
 def main():
-    if len(sys.argv) != 3:
-        print("usage: gen_key.py <secret filename> <public filename>")
+    if len(sys.argv) != 2:
+        print("usage: gen_key.py <secret filename>")
+        print("... creates an SECP384R1 elliptic curve key in PKCS8 PEM format.")
         sys.exit(1)
 
-    _signing_key, _verifying_key = ed25519.create_keypair()
+    pk = ec.generate_private_key(ec.SECP256R1(), default_backend())
+    pem = pk.private_bytes(encoding=Encoding.PEM,
+                           format=PrivateFormat.PKCS8,
+                           encryption_algorithm=NoEncryption()
+                           )
+
     with open(sys.argv[1], "wb") as f:
-        f.write(_signing_key.to_bytes())
-
-    with open(sys.argv[2], "wb") as f:
-        f.write(_verifying_key.to_bytes())
-
-    vkey_hex = _verifying_key.to_ascii(encoding="hex")
-    print("Generated public key: '{}'".format(vkey_hex.decode()))
+        f.write(pem)
 
 
 if __name__ == '__main__':
