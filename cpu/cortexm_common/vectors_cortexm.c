a69049c5f0532ef8712207d56739a78e3afe0a68
Kaspar Schleiser <kaspar@schleiser.de>
2020-10-21 14:09:24 +0200

    cpu/cortexm_common: add riotcore_rs hacks

--- cpu/cortexm_common/vectors_cortexm.c
+++ cpu/cortexm_common/vectors_cortexm.c
@@ -26,6 +26,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#ifndef MODULE_RIOTCORE_RS
 #include "cpu.h"
 #include "periph_cpu.h"
 #include "kernel_init.h"
@@ -545,3 +546,4 @@ ISR_VECTOR(0) const cortexm_base_t cortex_vector_base = {
 #endif
     }
 };
+#endif /* MODULE_RIOTCORE_RS */
