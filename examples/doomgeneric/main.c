b7028f80284cbfa37ff3d4a91d22c1046f8d5ce2
Kaspar Schleiser <kaspar@schleiser.de>
2021-09-02 23:01:54 +0200

    WIP

--- examples/doomgeneric/main.c
+++ examples/doomgeneric/main.c
@@ -1,28 +1,58 @@
-/*
- * Copyright (C) 2021 Kaspar Schleiser <kaspar@schleiser.de>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
+//
+// Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 2005-2014 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	Main program, simply calls D_DoomMain high level loop.
+//
 
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       DOOM
- *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- *
- * @}
- */
+//#include "config.h"
 
 #include <stdio.h>
-#include <string.h>
+
+//#include "doomtype.h"
+//#include "i_system.h"
+//#include "m_argv.h"
+
+//
+// D_DoomMain()
+// Not a globally visible function, just included for source reference,
+// calls all startup code, parses command line options.
+//
+
+void D_DoomMain (void);
+
+void M_FindResponseFile(void);
+
+void dg_Create(void);
 
 
 int main(void)
 {
+    // save arguments
+
+ //   myargc = "";
+  //  myargv = "";
+
+    M_FindResponseFile();
+
+    // start doom
+    printf("Starting D_DoomMain\r\n");
+    
+	dg_Create();
+
+	D_DoomMain ();
+
     return 0;
 }
+
