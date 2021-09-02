794c09c1b68fb6868dab1286c14318a26f39bc2b
Kaspar Schleiser <kaspar@schleiser.de>
2021-09-02 13:58:17 +0200

    WIP

---
+++ examples/doomgeneric/main.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       DOOM
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+    return 0;
+}
