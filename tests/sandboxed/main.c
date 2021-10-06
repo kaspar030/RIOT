d6a6f95d4deab7bf4fcf4209e51d9f6cdfcb9ab3
Kaspar Schleiser <kaspar@schleiser.de>
2021-10-06 13:15:55 +0200

    fixup! WIP tests/sandboxed

--- tests/sandboxed/main.c
+++ tests/sandboxed/main.c
@@ -18,22 +18,57 @@
  *
  * @}
  */
+#include <stdio.h>
+
+#include "cpu.h"
 #include "thread.h"
+#include "sandbox.h"
 
 static thread_t sandboxed;
-static char stack[THREAD_STACKSIZE_MAIN];
+static sandbox_t sandbox;
+
+static char mem[THREAD_STACKSIZE_MAIN * 2];
+
+bool is_privileged(void)
+{
+    return (__get_IPSR()>0) || ((__get_CONTROL() & CONTROL_nPRIV_Msk) == 0);
+}
 
 static void *_sandboxed(void *arg)
 {
     (void)arg;
 
     puts("_sandboxed()");
+    if (is_privileged()) {
+        puts("priviledged");
+    }
+    else {
+        puts("unpriviledged");
+    }
     return 0;
+}
+
+kernel_pid_t thread_create_sandboxed(thread_t *thread, sandbox_t *sandbox, char *mem_start,
+                                    size_t mem_len, size_t stack_size, uint8_t priority,
+                                    int flags, thread_task_func_t function,
+                                    void *arg,
+                                    const char *name)
+{
+    sandbox->mem_start = mem_start;
+    sandbox->mem_len = mem_len;
+
+    assert(stack_size <= mem_len);
+
+    return thread_create_detached(thread, mem_start, stack_size,
+                                              priority,
+                                              flags,
+                                              function, arg, name);
+
 }
 
 int main(void)
 {
-    kernel_pid_t pid = thread_create_detached(&sandboxed, stack, sizeof(stack),
+    kernel_pid_t pid = thread_create_sandboxed(&sandboxed, &sandbox, mem, sizeof(mem), THREAD_STACKSIZE_MAIN,
                                               THREAD_PRIORITY_MAIN,
                                               THREAD_CREATE_STACKTEST,
                                               _sandboxed, NULL, "sandboxed");
