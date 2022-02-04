32347831665effd532ec4ae121779ee6d1e54fb0
Kaspar Schleiser <kaspar@schleiser.de>
2022-02-04 10:10:19 +0100

    REMOVEME: some change

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -23,7 +23,9 @@
 
 int main(void)
 {
+
     puts("Hello World!");
+
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
