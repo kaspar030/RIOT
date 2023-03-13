7a66e2e25b1dbcbc572ed2ddb6118183912fd4e6
Kaspar Schleiser <kaspar@schleiser.de>
2023-03-13 10:11:39 +0100

    core/thread_events: introduce `thread_event_post_and_get_this()`

--- core/include/thread_events.h
+++ core/include/thread_events.h
@@ -16,6 +16,7 @@ thread_event_t *thread_event_get_oneof(thread_event_t *events, size_t num);
 static inline thread_event_t *thread_event_get_this(thread_event_t *event) {
     return thread_event_get_oneof(event, 1);
 }
+thread_event_t *thread_event_post_and_get_this(thread_t *thread, thread_event_t *to_post, thread_event_t *this);
 
 thread_event_t *thread_event_tryget(void);
 thread_event_t *thread_event_tryget_this(thread_event_t *events);
--- core/thread_event.c
+++ core/thread_event.c
@@ -1,6 +1,9 @@
 #include "irq.h"
 #include "thread.h"
 
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
 thread_event_t *thread_event_get(void)
 {
     thread_t *me = thread_get_active();
@@ -31,9 +34,11 @@ thread_event_t *thread_event_get_oneof(thread_event_t *wanted_events,
     clist_node_t deferred = { .next = NULL };
 
     do {
-        unsigned state = irq_disable();
+        DEBUG("%s: looping\n", __func__);
+        irq_disable();
         /* if there's any event ... */
-        if ((thread_event = (thread_event_t *)clist_lpop(&me->thread_events))) {
+        if ((thread_event =
+                    (thread_event_t *)clist_lpop(&me->thread_events))) {
             /* check if it is in our array of wanted events */
             if (thread_event >= &wanted_events[0] &&
                 (thread_event < &wanted_events[wanted_events_numof])) {
@@ -41,7 +46,7 @@ thread_event_t *thread_event_get_oneof(thread_event_t *wanted_events,
                  * thread_events */
                 clist_concat(&deferred, &me->thread_events);
                 me->thread_events.next = deferred.next;
-                irq_restore(state);
+                irq_enable();
                 break;
             }
             else {
@@ -51,12 +56,56 @@ thread_event_t *thread_event_get_oneof(thread_event_t *wanted_events,
         }
         sched_set_status(me, STATUS_EVENT_BLOCKED);
         thread_yield_higher();
-        irq_restore(state);
+        irq_enable();
     } while (1);
 
     return thread_event;
 }
 
+thread_event_t *thread_event_post_and_get_this(thread_t *thread,
+                                               thread_event_t *to_post,
+                                               thread_event_t *this)
+{
+    thread_t *me = thread_get_active();
+    thread_event_t *thread_event;
+
+    clist_node_t deferred = { .next = NULL };
+
+    irq_disable();
+    DEBUG("%s: posting\n", __func__);
+    thread_event_post_noirqs(thread, to_post);
+
+    do {
+        DEBUG("%s: looping\n", __func__);
+        irq_disable();
+        /* if there's any event ... */
+        while ((thread_event =
+                    (thread_event_t *)clist_lpop(&me->thread_events))) {
+            /* check if it is in our array of wanted events */
+            if (thread_event == this) {
+                DEBUG("%s: got my event\n", __func__);
+                /* it is. push the deferred events to the front of our
+                 * thread_events */
+                clist_concat(&deferred, &me->thread_events);
+                me->thread_events.next = deferred.next;
+                irq_enable();
+                goto out;
+            }
+            else {
+                DEBUG("%s: deferring unwanted event\n", __func__);
+                /* unwanted event, add to deferred list */
+                clist_rpush(&deferred, &thread_event->next);
+            }
+        }
+        sched_set_status(me, STATUS_EVENT_BLOCKED);
+        thread_yield_higher();
+        irq_enable();
+    } while (1);
+out:
+    DEBUG("%s: done\n", __func__);
+
+    return thread_event;
+}
 thread_event_t *thread_event_tryget(void)
 {
     thread_event_t *thread_event;
